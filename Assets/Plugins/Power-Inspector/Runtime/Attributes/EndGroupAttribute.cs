5b62cf8cb74a0a7c9d959e713b87eec0cb8ff01a
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2024-12-18 12:58:30 +0500

    Moved EndGroup Attribute inside PowerEditor.Attributes namespace

--- Assets/Plugins/Power-Inspector/Runtime/Attributes/EndGroupAttribute.cs
+++ Assets/Plugins/Power-Inspector/Runtime/Attributes/EndGroupAttribute.cs
@@ -1,15 +1,18 @@
 using System;
 
-[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
-public class EndGroupAttribute : InspectorAttribute
+namespace PowerEditor.Attributes
 {
-    public int openGroupsToClose;
-
-    public EndGroupAttribute(int openGroupsToClose = 1)
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class EndGroupAttribute : InspectorAttribute
     {
-        if (openGroupsToClose < 1)
-            openGroupsToClose = 1;
+        public int openGroupsToClose;
 
-        this.openGroupsToClose = openGroupsToClose;
+        public EndGroupAttribute(int openGroupsToClose = 1)
+        {
+            if (openGroupsToClose < 1)
+                openGroupsToClose = 1;
+
+            this.openGroupsToClose = openGroupsToClose;
+        }
     }
 }
