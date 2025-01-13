917916ae62e0dd9f0e8851d4abdb05ab2053f6ba
AbdurRafay-Nasir <AbdurRafayNasir1@gmail.com>
2025-01-13 21:36:39 +0500

    Added Paddng Margin options in FoldoutGroup

--- Assets/Plugins/Power-Inspector/Attributes/Runtime/Group/FoldoutGroupAttribute.cs
+++ Assets/Plugins/Power-Inspector/Attributes/Runtime/Group/FoldoutGroupAttribute.cs
@@ -5,19 +5,87 @@ using UnityEngine.UIElements;
 namespace PowerTools.Attributes
 {
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
-    public class FoldoutGroupAttribute : PropertyAttribute, IGroupAttribute, IPaddingMargin
+    public class FoldoutGroupAttribute : PropertyAttribute, IGroupAttribute
     {
-        private readonly string name;
-        private readonly bool open;
+        #region Padding
 
         public float PaddingLeft { get; set; }
-        public float PaddingRight { get; set; }
-        public float PaddingTop { get; set; }
-        public float PaddingBottom { get; set; }
+        public float PaddingRight { get; set; } = 2f;
+        public float PaddingTop { get; set; } = 2f;
+        public float PaddingBottom { get; set; } = 2f;
+        public float PaddingHorizontal
+        {
+            set
+            {
+                PaddingLeft = value;
+                PaddingRight = value;
+            }
+        }
+        public float PaddingVertical
+        {
+            set
+            {
+                PaddingTop = value;
+                PaddingBottom = value;
+            }
+        }
+        public float Padding
+        {
+            set
+            {
+                PaddingLeft = value;
+                PaddingRight = value;
+                PaddingTop = value;
+                PaddingBottom = value;
+            }
+        }
+
+        #endregion
+
+        #region Margin
+
         public float MarginLeft { get; set; }
         public float MarginRight { get; set; }
         public float MarginTop { get; set; }
         public float MarginBottom { get; set; }
+        public float MarginHorizontal
+        {
+            set
+            {
+                MarginLeft = value;
+                MarginRight = value;
+            }
+        }
+        public float MarginVertical
+        {
+            set
+            {
+                MarginTop = value;
+                MarginBottom = value;
+            }
+        }
+        public float Margin
+        {
+            set
+            {
+                MarginLeft = value;
+                MarginRight = value;
+                MarginTop = value;
+                MarginBottom = value;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Text displayed after foldout arrow
+        /// </summary>
+        private readonly string name;
+
+        /// <summary>
+        /// Initial state of Foldout. Defaults to false
+        /// </summary>
+        private readonly bool open;
 
         public FoldoutGroupAttribute(string name, bool open = false)
         {
